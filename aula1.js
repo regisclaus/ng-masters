f97ef2737abd77a5c396b8912bac0e5a47949536
regisclaus <regis.claus@gmail.com>
2014-07-27 00:33:57 -0300

    aula1.js

---
+++ aula1.js
@@ -0,0 +1,10 @@
+function vaibuscarasissues(){
+    function GithubRepo(username, reponame){
+        //seu codigo aqui...
+    }
+
+    //mais um pouco de codigo aqui....
+
+    var le_repo = GithubRepo(username, reponame);
+    le_repo.busca_issues_e_popula_tabela("#issuestable");
+}
